af6b59a1234c7f61e58103b56c292bd7af069655
Anikr04 <73298015+Anikr04@users.noreply.github.com>
2022-03-12 22:37:30 +0530

    Add files via upload

---
+++ reactlearnapp/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Temp from './component/weather/temp';
+//import Resturant from './component/Basics/Resturant';
+//import UseState from './component/Hooks/useState';
+//import UseEffect from './component/Hooks/useEffects';
+//import UseReducer from './component/Hooks/useReducer';
+//import Todo from './component/todoreact/todo';
+
+const App = () => {
+  return (
+    <>
+    <Temp/>
+    </>
+  )
+}
+
+export default App
